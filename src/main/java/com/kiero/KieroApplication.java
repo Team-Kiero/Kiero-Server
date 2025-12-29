39aaa28061977907f4910c81ea9702ae93111c61
seahee <lollol0617@gmail.com>
2025-12-30 02:17:20 +0900

    [#1] feat: application 및 baseEntity 적용

---
+++ src/main/java/com/kiero/KieroApplication.java
@@ -0,0 +1,11 @@
+package com.kiero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KieroApplication.class, args);
+	}
+}
