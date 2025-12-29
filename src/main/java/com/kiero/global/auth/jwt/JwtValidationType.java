01f5b4ed06787bdf83056f463fa8f57c55c2be54
seahee <lollol0617@gmail.com>
2025-12-30 02:17:02 +0900

    [#1] feat: jwt 관련 설정 적용

---
+++ src/main/java/com/kiero/global/auth/jwt/JwtValidationType.java
@@ -0,0 +1,22 @@
+package com.kiero.global.auth.jwt;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum JwtValidationType {
+	VALID_JWT,
+	INVALID_JWT_SIGNATURE,
+	INVALID_JWT_TOKEN,
+	EXPIRED_JWT_TOKEN,
+	UNSUPPORTED_JWT_TOKEN,
+	EMPTY_JWT
+	;
+
+	public boolean isValid() {
+		return this == VALID_JWT;
+	}
+
+	public boolean isNotValid() {
+		return !isValid();
+	}
+}
