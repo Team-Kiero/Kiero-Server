a70674dc591c5951d83c51dbe993da5c54efdd4b
seahee <lollol0617@gmail.com>
2025-12-31 01:46:50 +0900

    [#7] feat: 구현을 위한 각종 enum클래스 작성

---
+++ src/main/java/com/kiero/global/auth/client/enums/Provider.java
@@ -0,0 +1,6 @@
+package com.kiero.global.auth.client.enums;
+
+public enum Provider {
+	KAKAO
+
+}
---
+++ src/main/java/com/kiero/parent/exception/ParentSuccessCode.java
@@ -0,0 +1,21 @@
+package com.kiero.parent.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.kiero.global.response.base.BaseCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ParentSuccessCode implements BaseCode {
+	/*
+	200 OK
+	 */
+	LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공하였습니다."),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
