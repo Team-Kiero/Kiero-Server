483ca708676619f3ee17a8e358c371cd22156047
seahee <lollol0617@gmail.com>
2025-12-31 01:46:36 +0900

    [#7] feat: parent 엔티티의 jpa 매핑 테이블명 관리클래스 작성

---
+++ src/main/java/com/kiero/parent/domain/ParentTableConstants.java
@@ -0,0 +1,13 @@
+package com.kiero.parent.domain;
+
+public class ParentTableConstants {
+	public final static String TABLE_PARENT = "parent";
+	public final static String COLUMN_ID = "id";
+	public final static String COLUMN_NAME = "name";
+	public final static String COLUMN_EMAIL = "email";
+	public final static String COLUMN_ROLE = "role";
+	public final static String COLUMN_PROVIDER = "provider";
+	public final static String COLUMN_SOCIAL_ID = "social_id";
+	public final static String COLUMN_CREATED_AT = "created_at";
+	public final static String COLUMN_IMAGE = "image";
+}
