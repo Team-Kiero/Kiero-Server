2d6bf85c302540114e3d46133582d54ce170234c
seahee <lollol0617@gmail.com>
2025-12-31 17:57:15 +0900

    [#10] feat: mission 엔티티 생성

---
+++ src/main/java/com/kiero/mission/domain/Mission.java
@@ -0,0 +1,67 @@
+package com.kiero.mission.domain;
+
+import java.time.LocalDate;
+
+import com.kiero.child.domain.Child;
+import com.kiero.global.entity.BaseTimeEntity;
+import com.kiero.parent.domain.Parent;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = MissionTableConstants.TABLE_MISSION)
+public class Mission extends BaseTimeEntity {
+
+	@Id
+	@Column(name = MissionTableConstants.COLUMN_ID)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = MissionTableConstants.COLUMN_NAME, nullable = false)
+	private String name;
+
+	@Column(name = MissionTableConstants.COLUMN_REWARD, nullable = false)
+	private int reward;
+
+	@Column(name = MissionTableConstants.COLUMN_DUE_AT, nullable = false)
+	private LocalDate dueAt;
+
+	@Column(name = MissionTableConstants.COLUMN_IS_COMPLETED, nullable = false)
+	private Boolean isCompleted;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = MissionTableConstants.COLUMN_PARENT_ID, nullable = false)
+	private Parent parent;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = MissionTableConstants.COLUMN_CHILD_ID, nullable = false)
+	private Child child;
+
+	public static Mission create(Parent parent, Child child, String name, int reward, LocalDate dueAt) {
+		return Mission.builder()
+			.parent(parent)
+			.child(child)
+			.name(name)
+			.reward(reward)
+			.dueAt(dueAt)
+			.isCompleted(false)
+			.build();
+	}
+}
---
+++ src/main/java/com/kiero/mission/domain/MissionTableConstants.java
@@ -0,0 +1,12 @@
+package com.kiero.mission.domain;
+
+public class MissionTableConstants {
+	public static final String TABLE_MISSION = "mission";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_REWARD = "reward";
+	public static final String COLUMN_DUE_AT = "due_at";
+	public static final String COLUMN_IS_COMPLETED = "is_completed";
+	public static final String COLUMN_PARENT_ID = "parent_id";
+	public static final String COLUMN_CHILD_ID = "child_id";
+}
