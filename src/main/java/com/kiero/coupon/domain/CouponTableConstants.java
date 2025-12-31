34a8fcdd2d90e400994f4d89a74322705ce2cd86
seahee <lollol0617@gmail.com>
2025-12-31 17:56:57 +0900

    [#10] feat: coupon 엔티티 생성

---
+++ src/main/java/com/kiero/coupon/domain/CouponTableConstants.java
@@ -0,0 +1,8 @@
+package com.kiero.coupon.domain;
+
+public class CouponTableConstants {
+	public static final String TABLE_COUPON = "coupon";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_PRICE = "price";
+}
