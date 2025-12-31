db4392dfe84cec6c61b675754f29f46b2764503c
seahee <lollol0617@gmail.com>
2025-12-31 17:56:38 +0900

    [#10] feat: child 엔티티 생성

---
+++ src/main/java/com/kiero/child/domain/Child.java
@@ -0,0 +1,42 @@
+package com.kiero.child.domain;
+
+import com.kiero.global.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = ChildTableConstants.TABLE_CHILD)
+public class Child extends BaseTimeEntity {
+
+	@Id
+	@Column(name = ChildTableConstants.COLUMN_ID)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = ChildTableConstants.COLUMN_NAME, nullable = false)
+	private String name;
+
+	@Column(name = ChildTableConstants.COLUMN_COIN_AMOUNT, nullable = false)
+	private int coinAmount;
+
+	public static Child create(final String name) {
+		return Child.builder()
+			.name(name)
+			.coinAmount(0)
+			.build();
+	}
+}
