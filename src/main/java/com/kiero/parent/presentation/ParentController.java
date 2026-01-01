949a73eec0b62be77c7f8139a1017818a11f2f35
seahee <lollol0617@gmail.com>
2026-01-01 22:19:46 +0900

    [#8] feat: 액세스 코드 재발행 기능 구현

--- src/main/java/com/kiero/parent/presentation/ParentController.java
+++ src/main/java/com/kiero/parent/presentation/ParentController.java
@@ -3,6 +3,7 @@ package com.kiero.parent.presentation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kiero.global.auth.annotation.CurrentMember;
 import com.kiero.global.auth.client.dto.SocialLoginRequest;
+import com.kiero.global.auth.jwt.dto.AccessTokenGenerateResponse;
 import com.kiero.global.auth.jwt.service.TokenService;
 import com.kiero.global.response.dto.SuccessResponse;
 import com.kiero.parent.exception.ParentSuccessCode;
 import com.kiero.parent.presentation.dto.ParentLoginResponse;
+import com.kiero.parent.service.AuthService;
 import com.kiero.parent.service.ParentService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,6 +34,7 @@ public class ParentController {
 
 	private final ParentService parentService;
 	private final TokenService tokenService;
+	private final AuthService authService;
 
 	@PostMapping("/login")
 	public ResponseEntity<SuccessResponse<ParentLoginResponse>> login(
@@ -76,6 +80,15 @@ public class ParentController {
 		tokenService.deleteRefreshToken(memberId);
 		return ResponseEntity.ok()
 			.body(SuccessResponse.of(ParentSuccessCode.LOGOUT_SUCCESS));
+	}
+
+	@PostMapping("/reissue/access-token")
+	public ResponseEntity<SuccessResponse<AccessTokenGenerateResponse>> reissueAccessToken(
+		@CookieValue("refreshToken") String refreshToken
+	) {
+		AccessTokenGenerateResponse response = authService.generateAccessTokenFromRefreshToken(refreshToken);
+		return ResponseEntity.ok()
+			.body(SuccessResponse.of(ParentSuccessCode.ACCESS_TOKEN_REISSUE_SUCCESS, response));
 	}
 
 }
