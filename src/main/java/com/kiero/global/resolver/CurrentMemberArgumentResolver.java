901af8c5d15571fd7778610710c3425ca730dda3
seahee <lollol0617@gmail.com>
2026-01-01 23:52:50 +0900

    [#8] feat: @CurrentMember 어노테이션의 리졸버가 memberId, role이 담긴 CurrentAuth 객체를 반환하도록 설정

--- src/main/java/com/kiero/global/resolver/CurrentMemberArgumentResolver.java
+++ src/main/java/com/kiero/global/resolver/CurrentMemberArgumentResolver.java
@@ -2,6 +2,7 @@ package com.kiero.global.resolver;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -10,6 +11,10 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import com.kiero.global.auth.annotation.CurrentMember;
+import com.kiero.global.auth.dto.CurrentAuth;
+import com.kiero.global.auth.enums.Role;
+import com.kiero.global.auth.jwt.exception.TokenErrorCode;
+import com.kiero.global.exception.KieroException;
 
 @Component
 public class CurrentMemberArgumentResolver implements HandlerMethodArgumentResolver {
@@ -17,7 +22,7 @@ public class CurrentMemberArgumentResolver implements HandlerMethodArgumentResol
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		return parameter.getParameterAnnotation(CurrentMember.class) != null
-			&& parameter.getParameterType().equals(Long.class);
+			&& parameter.getParameterType().equals(CurrentAuth.class);
 	}
 
 	@Override
@@ -27,6 +32,14 @@ public class CurrentMemberArgumentResolver implements HandlerMethodArgumentResol
 		if (authentication == null || !authentication.isAuthenticated()) {
 			return null;
 		}
-		return Long.valueOf(authentication.getPrincipal().toString());
+		Long memberId = Long.valueOf(authentication.getPrincipal().toString());
+		Role role = authentication.getAuthorities().stream()
+			.map(GrantedAuthority::getAuthority)
+			.map(a-> a.replace("ROLE_", ""))
+			.map(Role::valueOf)
+			.findFirst()
+			.orElseThrow(()-> new KieroException(TokenErrorCode.INVALID_REFRESH_TOKEN_ERROR));
+		return new CurrentAuth(memberId, role);
 	}
+
 }
