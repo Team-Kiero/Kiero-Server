ccf7e72666f78564d35afba4d59a101124b80de7
seahee <lollol0617@gmail.com>
2026-01-01 23:51:06 +0900

    [#8] feat: Token의 id를 String으로 변경

--- src/main/java/com/kiero/global/auth/redis/Token.java
+++ src/main/java/com/kiero/global/auth/redis/Token.java
@@ -3,28 +3,29 @@ package com.kiero.global.auth.redis;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
+import com.kiero.global.auth.enums.Role;
+import com.kiero.global.auth.redis.util.TokenKeyGenerator;
+
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @RedisHash(value = "refreshToken", timeToLive = 60L * 60 * 24 * 14)
 @Getter
+@AllArgsConstructor(staticName = "of")
 public class Token {
 
 	@Id
-	private Long id;
+	private String id;
 
-	@Indexed
+	private Long memberId;
+
 	private String refreshToken;
 
-	protected Token() {
-	}
+	private String role;
 
-	public Token(Long id, String refreshToken) {
-		this.id = id;
-		this.refreshToken = refreshToken;
-	}
-
-	public static Token of(Long id, String refreshToken) {
-		return new Token(id, refreshToken);
+	public static Token of(Long memberId, String refreshToken, Role role) {
+		String key = TokenKeyGenerator.refreshKey(memberId, role);
+		return new Token(key, memberId,refreshToken, role.name());
 	}
 }
