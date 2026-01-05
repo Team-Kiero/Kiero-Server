c80105b5e2e6b2317bf17d727c6679a87dbc6a81
seahee <lollol0617@gmail.com>
2026-01-05 11:55:27 +0900

    [#18] feat: 자식-부모 유효성 검사를 위한 repository 메서드 작성 & 에러코드 작성

--- src/main/java/com/kiero/parent/exception/ParentErrorCode.java
+++ src/main/java/com/kiero/parent/exception/ParentErrorCode.java
@@ -11,6 +11,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ParentErrorCode implements BaseCode {
 	/*
+	403 FORBIDDEN
+	 */
+	NOT_ALLOWED_TO_CHILD(HttpStatus.FORBIDDEN, "자신의 아이에만 접근할 수 있습니다."),
+
+	/*
 	404 NOT FOUND
 	 */
 	PARENT_NOT_FOUND(HttpStatus.NOT_FOUND, "부모 정보를 찾을 수 없습니다."),
--- src/main/java/com/kiero/parent/repository/ParentChildRepository.java
+++ src/main/java/com/kiero/parent/repository/ParentChildRepository.java
@@ -1,5 +1,7 @@
 package com.kiero.parent.repository;
 
+import com.kiero.child.domain.Child;
+import com.kiero.parent.domain.Parent;
 import com.kiero.parent.domain.ParentChild;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,6 @@ public interface ParentChildRepository extends JpaRepository<ParentChild, Long> 
 
     @Query("SELECT pc.child.id FROM ParentChild pc WHERE pc.parent.id = :parentId")
     List<Long> findChildIdsByParentId(@Param("parentId") Long parentId);
+
+    boolean existsByParentAndChild(Parent parent, Child child);
 }
