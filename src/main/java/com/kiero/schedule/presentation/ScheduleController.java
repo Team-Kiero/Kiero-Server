2d6c59380cea9c4ca6fca3d225b12d90470f9dae
seahee <lollol0617@gmail.com>
2026-01-05 11:58:13 +0900

    [#18] feat: 일정 조회 api 컨트롤러, 서비스 단 구현

--- src/main/java/com/kiero/schedule/presentation/ScheduleController.java
+++ src/main/java/com/kiero/schedule/presentation/ScheduleController.java
@@ -1,17 +1,22 @@
 package com.kiero.schedule.presentation;
 
+import java.time.LocalDate;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kiero.global.auth.annotation.CurrentMember;
 import com.kiero.global.auth.dto.CurrentAuth;
 import com.kiero.global.response.dto.SuccessResponse;
 import com.kiero.schedule.exception.ScheduleSuccessCode;
 import com.kiero.schedule.presentation.dto.ScheduleAddRequest;
+import com.kiero.schedule.presentation.dto.ScheduleTabResponse;
 import com.kiero.schedule.service.ScheduleService;
 
 import jakarta.validation.Valid;
@@ -33,5 +38,17 @@ public class ScheduleController {
 		scheduleService.addSchedule(request, currentAuth.memberId(), childId);
 		return ResponseEntity.ok()
 			.body(SuccessResponse.of(ScheduleSuccessCode.SCHEDULE_CREATED));
+	}
+
+	@GetMapping("{childId}")
+	public ResponseEntity<SuccessResponse<ScheduleTabResponse>> getSchedules(
+		@RequestParam LocalDate startDate,
+		@RequestParam LocalDate endDate,
+		@PathVariable Long childId,
+		@CurrentMember CurrentAuth currentAuth
+	) {
+		ScheduleTabResponse response = scheduleService.getSchedules(startDate, endDate, currentAuth.memberId(), childId);
+		return ResponseEntity.ok()
+			.body(SuccessResponse.of(ScheduleSuccessCode.SCHEDULE_TAB_GET_SUCCESS, response));
 	}
 }
