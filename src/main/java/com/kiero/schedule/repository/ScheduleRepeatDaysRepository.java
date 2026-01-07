9799bdffe70e5fbe30e27911dd77e2807b0ed58f
seahee <lollol0617@gmail.com>
2026-01-07 22:20:47 +0900

    [#18] fix(ScheduleDetailRepository): 잘못된 Param 라이브러리 임포트 변경

--- src/main/java/com/kiero/schedule/repository/ScheduleRepeatDaysRepository.java
+++ src/main/java/com/kiero/schedule/repository/ScheduleRepeatDaysRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.kiero.schedule.domain.ScheduleRepeatDays;
 
-import io.lettuce.core.dynamic.annotation.Param;
 
 @Repository
 public interface ScheduleRepeatDaysRepository extends JpaRepository<ScheduleRepeatDays, Long> {
