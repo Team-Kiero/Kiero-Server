1561b34d905926b6c5498bb607e00e7fa59ebfd9
dietken1 <dietken1@ajou.ac.kr>
2026-01-09 03:40:55 +0900

    [#36] fix: child 엔티티 구조 변경 (name -> lastName/firstName)

--- src/main/java/com/kiero/child/domain/ChildTableConstants.java
+++ src/main/java/com/kiero/child/domain/ChildTableConstants.java
@@ -3,7 +3,8 @@ package com.kiero.child.domain;
 public class ChildTableConstants {
 	public static final String TABLE_CHILD = "child";
 	public static final String COLUMN_ID = "id";
-	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_LAST_NAME = "last_name";
+	public static final String COLUMN_FIRST_NAME = "first_name";
     public static final String COLUMN_ROLE = "role";
 	public static final String COLUMN_COIN_AMOUNT = "coin_amount";
 }
