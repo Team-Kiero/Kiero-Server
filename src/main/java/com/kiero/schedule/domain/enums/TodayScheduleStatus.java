c2307cec5cab9f00f3d8dc08397e98f68a7b49b4
seahee <lollol0617@gmail.com>
2026-01-08 17:35:21 +0900

    [#31] feat(TodayScheduleStatus): 현재 일정의 상태를 나타내는 enum 클래스 추가

---
+++ src/main/java/com/kiero/schedule/domain/enums/TodayScheduleStatus.java
@@ -0,0 +1,10 @@
+package com.kiero.schedule.domain.enums;
+
+public enum TodayScheduleStatus {
+	NO_SCHEDULE,
+	FIRST_SCHEDULE,
+	NEXT_SCHEDULE_EXIST,
+	NOW_SCHEDULE_EXIST,
+	FIRE_NOT_LIT,
+	FIRE_LIT
+}
