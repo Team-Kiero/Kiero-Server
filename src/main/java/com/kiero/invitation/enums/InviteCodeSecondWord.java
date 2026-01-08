a3159b6169dba9f725523cb401df566a6546e28f
dietken1 <dietken1@ajou.ac.kr>
2026-01-08 16:44:09 +0900

    [#25] fix: 초대코드 구성 키워드 변경

--- src/main/java/com/kiero/invitation/enums/InviteCodeSecondWord.java
+++ src/main/java/com/kiero/invitation/enums/InviteCodeSecondWord.java
@@ -6,16 +6,21 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum InviteCodeSecondWord {
-    DOG("강아지"),
-    CAT("고양이"),
-    RABBIT("토끼"),
-    HAMSTER("햄스터"),
-    PARROT("앵무새"),
-    TURTLE("거북이"),
-    GOLDFISH("금붕어"),
-    SQUIRREL("다람쥐"),
-    GUINEA_PIG("기니피그"),
-    CAPYBARA("카피바라");
+    KNIGHT("기사"),
+    WARRIOR("용사"),
+    CAPTAIN("대장"),
+    WIZARD("마법사"),
+    EXPLORER("탐험가"),
+    GUARDIAN("수호자"),
+    FAIRY("요정"),
+    HERO("히어로"),
+    NINJA("닌자"),
+    RANGER("레인저"),
+    JEWEL("보석"),
+    TIGER("호랑이"),
+    EAGLE("독수리"),
+    GOBLIN("도깨비"),
+    ASTRONAUT("우주인");
 
     private final String value;
 }
