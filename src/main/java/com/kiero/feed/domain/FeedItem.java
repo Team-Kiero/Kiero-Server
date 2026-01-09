46e2b9113220db780aacf35a197d1265ecb8a404
seahee <lollol0617@gmail.com>
2026-01-10 03:10:45 +0900

    [#39] refactor(FeedItem): LocalDate인 date 필드를 LocalDateTime의 occurredAt으로 변경

--- src/main/java/com/kiero/feed/domain/FeedItem.java
+++ src/main/java/com/kiero/feed/domain/FeedItem.java
@@ -1,6 +1,6 @@
 package com.kiero.feed.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.kiero.child.domain.Child;
@@ -21,7 +21,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,8 +40,8 @@ public class FeedItem extends BaseTimeEntity {
 	@Column(name = FeedItemTableConstants.COLUMN_ID)
 	private Long id;
 
-	@Column(name = FeedItemTableConstants.COLUMN_DATE, nullable = false)
-	private LocalDate date;
+	@Column(name = FeedItemTableConstants.COLUMN_OCCURRED_AT, nullable = false)
+	private LocalDateTime occurredAt;
 
 	@Enumerated(EnumType.STRING)
 	@Column(name = FeedItemTableConstants.COLUMN_EVENT_TYPE, nullable = false)
@@ -63,14 +62,14 @@ public class FeedItem extends BaseTimeEntity {
 	public static FeedItem create(
 		Parent parent,
 		Child child,
-		LocalDate date,
+		LocalDateTime occurredAt,
 		EventType eventType,
 		JsonNode metadata
 	) {
 		return FeedItem.builder()
 			.parent(parent)
 			.child(child)
-			.date(date)
+			.occurredAt(occurredAt)
 			.eventType(eventType)
 			.metadata(metadata)
 			.build();
