6e59806790c4394d3bbfaa5f4f06bbdac55c2db4
seahee <lollol0617@gmail.com>
2026-01-10 03:10:49 +0900

    [#39] refactor(FeedItem): LocalDate인 date 필드를 LocalDateTime의 occurredAt으로 변경

--- src/main/java/com/kiero/feed/domain/FeedItemTableConstants.java
+++ src/main/java/com/kiero/feed/domain/FeedItemTableConstants.java
@@ -3,7 +3,7 @@ package com.kiero.feed.domain;
 public class FeedItemTableConstants {
 	public static final String TABLE_FEED_ITEM = "feed_item";
 	public static final String COLUMN_ID = "id";
-	public static final String COLUMN_DATE = "date";
+	public static final String COLUMN_OCCURRED_AT= "occurred_at";
 	public static final String COLUMN_EVENT_TYPE = "event_type";
 	public static final String COLUMN_METADATA = "metadata";
 	public static final String COLUMN_PARENT_ID = "parent_id";
