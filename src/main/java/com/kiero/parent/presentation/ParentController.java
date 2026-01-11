e4b8a59a358c7d40881e8a62c0b658b89e88f94d
dietken1 <dietken1@ajou.ac.kr>
2026-01-11 22:06:43 +0900

    [#41] fix: 아이 연동여부 조회 api 요청 데이터 검증 추가

--- src/main/java/com/kiero/parent/presentation/ParentController.java
+++ src/main/java/com/kiero/parent/presentation/ParentController.java
@@ -6,9 +6,11 @@ import com.kiero.parent.presentation.dto.InviteCodeCreateRequest;
 import com.kiero.parent.presentation.dto.InviteCodeCreateResponse;
 import com.kiero.parent.presentation.dto.InviteStatusResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +33,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("api/v1/parents")
 public class ParentController {
@@ -108,8 +111,8 @@ public class ParentController {
     @GetMapping("/invite/status")
     public ResponseEntity<SuccessResponse<InviteStatusResponse>> checkInviteStatus(
             @CurrentMember CurrentAuth currentAuth,
-            @RequestParam String childLastName,
-            @RequestParam String childFirstName
+            @RequestParam @NotBlank(message = "자녀의 성은 필수입니다.") String childLastName,
+            @RequestParam @NotBlank(message = "자녀의 성은 필수입니다.") String childFirstName
     ) {
         InviteStatusResponse response = parentService.checkInviteStatus(
                 currentAuth.memberId(),
