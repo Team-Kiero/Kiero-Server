28d75354878077a8c4fa0ccce69530de7863e6ea
dietken1 <dietken1@ajou.ac.kr>
2026-01-12 22:51:25 +0900

    [#57] refactor: 사용하지 않는 MissionItemResponse 정적 팩토리 메서드 삭제

--- src/main/java/com/kiero/mission/presentation/dto/MissionItemResponse.java
+++ src/main/java/com/kiero/mission/presentation/dto/MissionItemResponse.java
@@ -1,22 +1,11 @@
 package com.kiero.mission.presentation.dto;
 
-import com.kiero.mission.domain.Mission;
-
 public record MissionItemResponse(
     Long id,
     String name,
     int reward,
     boolean isCompleted
 ) {
-    public static MissionItemResponse from(Mission mission) {
-        return new MissionItemResponse(
-            mission.getId(),
-            mission.getName(),
-            mission.getReward(),
-            mission.isCompleted()
-        );
-    }
-
     public static MissionItemResponse from(MissionResponse response) {
         return new MissionItemResponse(
             response.id(),
