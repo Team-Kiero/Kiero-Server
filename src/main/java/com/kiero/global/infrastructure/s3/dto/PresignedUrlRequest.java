396bf856200b1b7cdf30fe6e01f607312b45ba4d
dietken1 <dietken1@ajou.ac.kr>
2026-01-12 19:40:10 +0900

    fix: 파일 종류 검증 추가

--- src/main/java/com/kiero/global/infrastructure/s3/dto/PresignedUrlRequest.java
+++ src/main/java/com/kiero/global/infrastructure/s3/dto/PresignedUrlRequest.java
@@ -1,5 +1,7 @@
 package com.kiero.global.infrastructure.s3.dto;
 
+import com.kiero.global.infrastructure.s3.enums.AllowedFileType;
+import com.kiero.global.infrastructure.s3.validation.ValidFileContentType;
 import jakarta.validation.constraints.NotBlank;
 
 public record PresignedUrlRequest(
@@ -7,6 +9,7 @@ public record PresignedUrlRequest(
 	String fileName,
 
 	@NotBlank(message = "Content Type을 입력해주세요.")
+    @ValidFileContentType(value = AllowedFileType.IMAGE, message = "이미지 파일만 업로드 가능합니다.")
 	String contentType
 ) {
 }
