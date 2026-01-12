0659fe04b0e5669c30cd67f36f981f009637cc99
dietken1 <dietken1@ajou.ac.kr>
2026-01-12 18:09:40 +0900

    [#51] fix: ErrorResponse 응답 형식 변경

--- src/main/java/com/kiero/global/response/dto/ErrorResponse.java
+++ src/main/java/com/kiero/global/response/dto/ErrorResponse.java
@@ -1,25 +1,30 @@
 package com.kiero.global.response.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.http.HttpStatus;
 
 import com.kiero.global.response.base.BaseCode;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
 	int status,
-	String message
+	String message,
+    String detail
 ) {
 
 	public static ErrorResponse of(BaseCode baseCode) {
-		return new ErrorResponse(baseCode.getHttpStatus().value(), baseCode.getMessage());
+		return new ErrorResponse(baseCode.getHttpStatus().value(), baseCode.getMessage(), null);
 	}
 
 	public static ErrorResponse of(HttpStatus httpStatus, String message) { //메시지 추가 커스텀
-		return new ErrorResponse(httpStatus.value(), message);
+		return new ErrorResponse(httpStatus.value(), message, null);
 	}
 
 	public static ErrorResponse of(BaseCode baseCode, Object detail) { //디테일 추가 커스텀
-		return new ErrorResponse(baseCode.getHttpStatus().value(),
-			baseCode.getMessage() + (detail != null ? ": " + detail : "")
+		return new ErrorResponse(
+                baseCode.getHttpStatus().value(),
+                baseCode.getMessage(),
+                detail != null ? detail.toString() : null
 		);
 	}
 }
