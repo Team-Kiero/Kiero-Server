4514c62b78bbae73dea0fc62dad357d02eae615b
sehee <132613722+sae2say@users.noreply.github.com>
2026-01-15 18:02:49 +0900

    [feat] #73 - schedule 서비스 유닛테스트 코드 작성 (#82)
    * [#73] feat: clock 사용을 위한 mockito 의존성 추가
    
    * [#73] feat: clock 추가 및 예외 케이스 처리 추가
    
    * [#73] feat: timeConfig 작성
    
    * [#73] feat: ScheduleService 유닛테스트 작성
    
    * [#73] fix: 오타 수정
    
    * [#73] fix: 오타 수정
    
    * [#73] fix: 오타 수정

--- src/main/java/com/kiero/schedule/exception/ScheduleErrorCode.java
+++ src/main/java/com/kiero/schedule/exception/ScheduleErrorCode.java
@@ -17,6 +17,10 @@ public enum ScheduleErrorCode implements BaseCode {
 	FIRE_LIT_ALREADY_COMPLETE(HttpStatus.BAD_REQUEST, "오늘의 불 피우기가 이미 완료되었습니다."),
 	SCHEDULE_ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "해당 일정은 이미 완료되었습니다."),
 	SCHEDULE_COULD_NOT_BE_SKIPPED(HttpStatus.BAD_REQUEST, "해당 일정은 다음으로 넘어갈 수 있는 상태가 아닙니다."),
+	DAY_OF_WEEK_XOR_DATE_REQUIRED(HttpStatus.BAD_REQUEST, "반복 요일이나 일정 중 한 가지만 입력되어야 합니다."),
+	DAY_OF_WEEK_NOT_NULLABLE_WHEN_IS_RECURRING_IS_TRUE(HttpStatus.BAD_REQUEST, "반복 일정일 때, 반복요일이 입력되어야 합니다."),
+	DATE_NOT_NULLABLE_WHEN_IS_RECURRING_IS_FALSE(HttpStatus.BAD_REQUEST, "반복 일정이 아닐 때, 일정 일자가 입력되어야 합니다."),
+	INVALID_DATE_DURATION(HttpStatus.BAD_REQUEST, "시작 일자가 종료 일자의 이전 시점이어야 합니다."),
 
 	/*
 	403 FORBIDDEN
