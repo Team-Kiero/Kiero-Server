e2b64a6b38666ef84fd50f83c99c0eb344751099
dietken1 <dietken1@ajou.ac.kr>
2026-01-18 18:11:26 +0900

    [#102] refactor: 공휴일 조회 쿼리 수정

--- src/main/java/com/kiero/holiday/repository/HolidayRepository.java
+++ src/main/java/com/kiero/holiday/repository/HolidayRepository.java
@@ -4,7 +4,9 @@ import com.kiero.holiday.domain.Holiday;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface HolidayRepository extends JpaRepository<Holiday, LocalDate> {
 
+    List<Holiday> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
