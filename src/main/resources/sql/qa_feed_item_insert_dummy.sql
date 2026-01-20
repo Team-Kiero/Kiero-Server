0fdb1a81cbab3477e0ffa52573b2bf6b3d90b77c
sehee <132613722+sae2say@users.noreply.github.com>
2026-01-21 08:20:12 +0900

    [#124] refactor: 피드쿼리 수정 (#125)

--- src/main/resources/sql/qa_feed_item_insert_dummy.sql
+++ src/main/resources/sql/qa_feed_item_insert_dummy.sql
@@ -11,22 +11,14 @@ VALUES
         'SCHEDULE',
         '{"content": "데모데이", "imageUrl": "https://kiero-bucket.s3.ap-northeast-2.amazonaws.com/schedule/Demoday.JPG"}',
         '2026-01-21 07:53:00.000000',
-        '2026-01-16 09:03:46.000000',
+        '2026-01-17 09:03:46.000000',
         :childId,
         :parentId
     ),
     (
         'COUPON',
-        '{"amount": 80, "content": "놀이동산 가기"}',
+        '{"amount": 50, "content": "놀이동산 가기"}',
         '2026-01-20 20:17:00.000000',
-        '2026-01-16 09:03:46.000000',
-        :childId,
-        :parentId
-    ),
-    (
-        'COMPLETE',
-        '{"amount": 10}',
-        '2026-01-20 17:15:22.000000',
         '2026-01-16 09:03:46.000000',
         :childId,
         :parentId
@@ -42,8 +34,24 @@ VALUES
     (
         'COMPLETE',
         '{"amount": 10}',
+        '2026-01-20 17:15:22.000000',
+        '2026-01-16 09:03:46.000000',
+        :childId,
+        :parentId
+    ),
+    (
+        'COMPLETE',
+        '{"amount": 10}',
         '2026-01-19 20:37:00.000000',
         '2026-01-13 09:03:46.000000',
+        :childId,
+        :parentId
+    ),
+    (
+        'SCHEDULE',
+        '{"content": "피아노 학원", "imageUrl": "https://kiero-bucket.s3.ap-northeast-2.amazonaws.com/schedule/piano.JPG"}',
+        '2026-01-19 13:55:00.000000',
+        '2026-01-12 13:55:00.000000',
         :childId,
         :parentId
     );
