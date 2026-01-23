8d143f62a7dd202cb010f5b38f81a19e39bd0a19
sehee <132613722+sae2say@users.noreply.github.com>
2026-01-23 20:36:52 +0900

    [#151] refactor: 시연영상용 더미데이터로 수정 (#152)

--- src/main/resources/sql/demo_feed_item_insert_dummy.sql
+++ src/main/resources/sql/demo_feed_item_insert_dummy.sql
@@ -8,22 +8,6 @@ INSERT INTO feed_item (
 )
 VALUES
     (
-        'SCHEDULE',
-        '{"content": "발표하기", "imageUrl": "https://kiero-bucket.s3.ap-northeast-2.amazonaws.com/schedule/%E1%84%8E%E1%85%AC%E1%84%80%E1%85%B3%E1%86%AB%E1%84%8B%E1%85%A7%E1%86%BC+%E1%84%87%E1%85%A1%E1%86%AF%E1%84%91%E1%85%AD%E1%84%89%E1%85%A1%E1%84%8C%E1%85%B5%E1%86%AB.jpeg"}',
-        '2026-01-23 11:05:00.000000',
-        '2026-01-17 09:03:46.000000',
-        :childId,
-        :parentId
-    ),
-    (
-        'SCHEDULE',
-        '{"content": "데모데이", "imageUrl": "https://kiero-bucket.s3.ap-northeast-2.amazonaws.com/schedule/Demoday.JPG"}',
-        '2026-01-23 07:53:00.000000',
-        '2026-01-17 09:03:46.000000',
-        :childId,
-        :parentId
-    ),
-    (
         'COUPON',
         '{"amount": 50, "content": "놀이동산 가기"}',
         '2026-01-22 20:17:00.000000',
