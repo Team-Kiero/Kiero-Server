1a1a4423bdca4d1415f00af64ee1a35f7a80221d
dietken1 <dietken1@ajou.ac.kr>
2026-02-22 01:14:47 +0900

    [#165] feat: 소원 쿠폰 구매 API Method 및 Path 변경(Patch -> Post)

--- src/main/java/com/kiero/coupon/adapter/in/web/CouponController.java
+++ src/main/java/com/kiero/coupon/adapter/in/web/CouponController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -96,7 +95,7 @@ public class CouponController {
 	}
 
 	@PreAuthorize("hasAnyRole('CHILD', 'ADMIN')")
-	@PatchMapping("/{couponId}")
+	@PostMapping("/{couponId}/purchase")
 	public ResponseEntity<SuccessResponse<CouponResponse>> purchaseCoupon(
 		@CurrentMember CurrentAuth currentAuth,
 		@PathVariable Long couponId
