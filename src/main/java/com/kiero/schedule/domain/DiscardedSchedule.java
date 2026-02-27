4453fb54f25065da7dbd7c8aee2a359ed4ee37e5
seahee <lollol0617@gmail.com>
2026-02-28 00:35:15 +0900

    [#162] refactor: enum타입 스트링으로 명시

--- src/main/java/com/kiero/schedule/domain/DiscardedSchedule.java
+++ src/main/java/com/kiero/schedule/domain/DiscardedSchedule.java
@@ -6,6 +6,8 @@ import com.kiero.schedule.domain.enums.DayOfWeek;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +37,7 @@ public class DiscardedSchedule {
 	@Column(name = DiscardedScheduleTableConstants.COLUMN_DATE, nullable = false)
 	LocalDate date;
 
+	@Enumerated(EnumType.STRING)
 	@Column(name = DiscardedScheduleTableConstants.COLUMN_DAY_OF_WEEK, nullable = false)
 	DayOfWeek dayOfWeek;
 
