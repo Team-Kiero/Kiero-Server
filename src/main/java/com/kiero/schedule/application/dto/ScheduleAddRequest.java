90440dc66b15052521954deeac9cd719efc62886
seahee <lollol0617@gmail.com>
2026-03-13 20:39:16 +0900

    [#199] refactor: 일정 등록/수정 시 필수 데이터가 비어있을 때 예외를 던지도록 수정

--- src/main/java/com/kiero/schedule/application/dto/ScheduleAddRequest.java
+++ src/main/java/com/kiero/schedule/application/dto/ScheduleAddRequest.java
@@ -5,15 +5,16 @@ import java.time.LocalTime;
 
 import com.kiero.schedule.domain.enums.ScheduleColor;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ScheduleAddRequest(
-	@Size(max = 10) @NotNull String name,
-	@NotNull Boolean isRecurring,
-	@NotNull LocalTime startTime,
-	@NotNull LocalTime endTime,
-	@NotNull ScheduleColor scheduleColor,
+	@Size(max = 10) @NotNull @NotEmpty String name,
+	@NotNull @NotEmpty Boolean isRecurring,
+	@NotNull @NotEmpty LocalTime startTime,
+	@NotNull @NotEmpty LocalTime endTime,
+	@NotNull @NotEmpty ScheduleColor scheduleColor,
 	LocalDate firstOrderDate,
 	String dayOfWeek,
 	String dates
--- src/main/java/com/kiero/schedule/application/dto/ScheduleModifyRequest.java
+++ src/main/java/com/kiero/schedule/application/dto/ScheduleModifyRequest.java
@@ -4,15 +4,16 @@ import java.time.LocalTime;
 
 import com.kiero.schedule.domain.enums.ScheduleColor;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ScheduleModifyRequest(
-	@Size(max = 10) @NotNull String name,
-	@NotNull Boolean isRecurring,
-	@NotNull LocalTime startTime,
-	@NotNull LocalTime endTime,
-	@NotNull ScheduleColor scheduleColor,
+	@Size(max = 10) @NotNull @NotEmpty String name,
+	@NotNull @NotEmpty Boolean isRecurring,
+	@NotNull @NotEmpty LocalTime startTime,
+	@NotNull @NotEmpty LocalTime endTime,
+	@NotNull @NotEmpty ScheduleColor scheduleColor,
 	String dayOfWeek,
 	String dates,
 	Boolean isIncludeFollowing
