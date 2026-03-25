8c9059d1d396d7ea9b8ae5fdbb224e215ea302eb
seahee <lollol0617@gmail.com>
2026-03-25 21:37:17 +0900

    [#220] feat: 캐시 Config 작성

---
+++ src/main/java/com/kiero/global/config/CacheConfig.java
@@ -0,0 +1,50 @@
+package com.kiero.global.config;
+
+import java.time.Duration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		ObjectMapper objectMapper = new ObjectMapper()
+			.registerModule(new JavaTimeModule())
+			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+			.activateDefaultTyping(
+				BasicPolymorphicTypeValidator.builder()
+					.allowIfBaseType(Object.class)
+					.build(),
+				ObjectMapper.DefaultTyping.EVERYTHING
+			);
+
+		GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+			.entryTtl(Duration.ofMinutes(5)) // 만료시간 5분
+			.serializeKeysWith( // key는 String으로 직렬화
+				RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith( // value는 json으로 직렬화
+				RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+			.disableCachingNullValues();
+
+		return RedisCacheManager.builder(connectionFactory)
+			.cacheDefaults(config)
+			.build();
+	}
+}
