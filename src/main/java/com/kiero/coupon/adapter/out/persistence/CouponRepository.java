fc8df89826141289c340dbf218a03c02fd4cd0f2
dietken1 <dietken1@ajou.ac.kr>
2026-04-07 23:28:21 +0900

    [#228] feat: (CouponRepository) 아이 및 부모별 쿠폰 조회 및 bulk 삭제 메서드 추가

--- src/main/java/com/kiero/coupon/adapter/out/persistence/CouponRepository.java
+++ src/main/java/com/kiero/coupon/adapter/out/persistence/CouponRepository.java
@@ -3,9 +3,22 @@ package com.kiero.coupon.adapter.out.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kiero.coupon.domain.Coupon;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 	List<Coupon> findAllByChildIdOrderByPriceAscUpdatedAtDesc(Long childId);
+
+	List<Coupon> findAllByChildId(Long childId);
+
+	@Modifying
+	@Query("DELETE FROM Coupon c WHERE c.child.id = :childId")
+	void deleteAllByChildId(@Param("childId") Long childId);
+
+	@Modifying
+	@Query("DELETE FROM Coupon c WHERE c.parent.id = :parentId")
+	void deleteAllByParentId(@Param("parentId") Long parentId);
 }
