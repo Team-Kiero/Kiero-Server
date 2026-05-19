964f936df3896b672bde749839cfa953c67abf1a
seahee <lollol0617@gmail.com>
2026-05-20 00:31:50 +0900

    [#236] feat: 필수 동의약관 조회 response dto 생성

--- src/main/java/com/kiero/terms/application/dto/RequiredTermsResponse.java
+++ src/main/java/com/kiero/terms/application/dto/RequiredTermsResponse.java
@@ -4,11 +4,13 @@ import com.kiero.terms.domain.Terms;
 import com.kiero.terms.domain.enums.TermsType;
 
 public record RequiredTermsResponse(
+	Long termsId,
 	TermsType termsType,
 	String url
 ) {
 	public static RequiredTermsResponse from(Terms terms) {
 		return new RequiredTermsResponse(
+			terms.getId(),
 			terms.getType(),
 			terms.getUrl()
 		);
