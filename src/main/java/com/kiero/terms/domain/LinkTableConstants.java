45d83360665fedb5cfe6b3d8c7be70993064ae36
seahee <lollol0617@gmail.com>
2026-05-19 23:15:39 +0900

    [#236] feat: Link 엔터티 작성

---
+++ src/main/java/com/kiero/terms/domain/LinkTableConstants.java
@@ -0,0 +1,8 @@
+package com.kiero.terms.domain;
+
+public class LinkTableConstants {
+	public static final String TABLE_LINK = "link";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_TYPE = "type";
+	public static final String COLUMN_URL = "url";
+}
---
+++ src/main/java/com/kiero/terms/domain/enums/LinkType.java
@@ -0,0 +1,5 @@
+package com.kiero.terms.domain.enums;
+
+public enum LinkType {
+	CUSTOMER_SUPPORT   // 고객 지원
+}
