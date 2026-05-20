62d12c7fa1d41080d7ff067825a3abf14bd8fb81
dietken1 <dietken1@ajou.ac.kr>
2026-05-20 23:11:34 +0900

    [#218] feat: (AppleLoginRequest) authorizationCode 필드 추가

--- src/main/java/com/kiero/parent/application/dto/AppleLoginRequest.java
+++ src/main/java/com/kiero/parent/application/dto/AppleLoginRequest.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 public record AppleLoginRequest(
 	@NotBlank(message = "identityToken이 입력되지 않았습니다.")
 	String identityToken,
+	@NotBlank(message = "authorizationCode가 입력되지 않았습니다.")
+	String authorizationCode,
 	String name
 ) {
 }
