ee0b1dce28aac3810f372d34152a1e28ddd04c47
seahee <lollol0617@gmail.com>
2026-05-20 14:13:13 +0900

    [#236] feat: 필수 동의약관 동의여부 조회 response dto 구성

--- src/main/java/com/kiero/terms/application/port/out/TermsAgreementLoadPort.java
+++ src/main/java/com/kiero/terms/application/port/out/TermsAgreementLoadPort.java
@@ -1,5 +1,8 @@
 package com.kiero.terms.application.port.out;
 
+import java.util.List;
+
 public interface TermsAgreementLoadPort {
 	boolean existsTermsAgreement(Long parentId, Long termsId);
+	List<Long> findActiveAgreedTermsIds(Long parentId);
 }
