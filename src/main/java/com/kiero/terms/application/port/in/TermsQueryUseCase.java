7de9678156ea51e7fd635e10643f3f59bca7691d
seahee <lollol0617@gmail.com>
2026-05-20 14:13:48 +0900

    [#236] feat: 필수 동의약관 동의여부 조회 port 구성

--- src/main/java/com/kiero/terms/application/port/in/TermsQueryUseCase.java
+++ src/main/java/com/kiero/terms/application/port/in/TermsQueryUseCase.java
@@ -3,7 +3,9 @@ package com.kiero.terms.application.port.in;
 import java.util.List;
 
 import com.kiero.terms.application.dto.RequiredTermsResponse;
+import com.kiero.terms.application.dto.TermsAgreementStatusResponse;
 
 public interface TermsQueryUseCase {
 	List<RequiredTermsResponse> getRequiredTerms();
+	TermsAgreementStatusResponse getRequiredTermsAgreementStatus(Long parentId);
 }
