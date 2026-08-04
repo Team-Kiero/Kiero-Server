9e5dc9d7b77ad3859445783b07da354921f04e66
seahee <lollol0617@gmail.com>
2026-08-04 23:08:23 +0900

    [refactor] #284 - 부모의 내 아이 조회 응답에 pk 추가

--- src/main/java/com/kiero/parent/application/dto/ChildInfoResponse.java
+++ src/main/java/com/kiero/parent/application/dto/ChildInfoResponse.java
@@ -3,11 +3,12 @@ package com.kiero.parent.application.dto;
 import com.kiero.child.domain.Child;
 
 public record ChildInfoResponse(
+        Long id,
         Long childId,
         String childLastName,
         String childFirstName
 ) {
     public static ChildInfoResponse of(Child child) {
-        return new ChildInfoResponse(child.getId(), child.getLastName(), child.getFirstName());
+        return new ChildInfoResponse(child.getId(), child.getId(), child.getLastName(), child.getFirstName());
     }
 }
