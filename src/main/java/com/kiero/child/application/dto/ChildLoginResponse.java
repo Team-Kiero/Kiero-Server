7b225fe6047b3af366adb01f70b3c68851e22f51
seahee <lollol0617@gmail.com>
2026-08-04 23:08:12 +0900

    [refactor] #284 - 아이 로그인 응답에 pk 추가

--- src/main/java/com/kiero/child/application/dto/ChildLoginResponse.java
+++ src/main/java/com/kiero/child/application/dto/ChildLoginResponse.java
@@ -3,13 +3,14 @@ package com.kiero.child.application.dto;
 import com.kiero.global.auth.enums.Role;
 
 public record ChildLoginResponse(
+        Long id,
         String lastName,
         String firstName,
         Role role,
         String accessToken,
         String refreshToken
 ) {
-    public static ChildLoginResponse of(String lastName, String firstName, Role role, String accessToken, String refreshToken) {
-        return new ChildLoginResponse(lastName, firstName, role, accessToken, refreshToken);
+    public static ChildLoginResponse of(Long id, String lastName, String firstName, Role role, String accessToken, String refreshToken) {
+        return new ChildLoginResponse(id, lastName, firstName, role, accessToken, refreshToken);
     }
 }
