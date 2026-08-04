b1f622a627035154a2c9898f143092b82ed27858
seahee <lollol0617@gmail.com>
2026-08-04 23:34:20 +0900

    [refactor] #284 - 내 아이 조회에 부모아이 연결 식별자 포함

--- src/main/java/com/kiero/parent/application/dto/ChildInfoResponse.java
+++ src/main/java/com/kiero/parent/application/dto/ChildInfoResponse.java
@@ -1,6 +1,7 @@
 package com.kiero.parent.application.dto;
 
 import com.kiero.child.domain.Child;
+import com.kiero.parent.domain.ParentChild;
 
 public record ChildInfoResponse(
         Long id,
@@ -8,7 +9,8 @@ public record ChildInfoResponse(
         String childLastName,
         String childFirstName
 ) {
-    public static ChildInfoResponse of(Child child) {
-        return new ChildInfoResponse(child.getId(), child.getId(), child.getLastName(), child.getFirstName());
+    public static ChildInfoResponse of(ParentChild parentChild) {
+        Child child = parentChild.getChild();
+        return new ChildInfoResponse(parentChild.getId(), child.getId(), child.getLastName(), child.getFirstName());
     }
 }
